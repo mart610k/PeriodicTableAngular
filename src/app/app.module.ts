fb6a8db8250af5f4b9636771f604cb0bbc1fe915
mart610k <martin-baage@hotmail.com>
2020-08-13 12:55:50 +0200

    added Modules and provider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PeriodicTableComponent } from './periodic-table/periodic-table.component';
+import { HttpClientModule } from "@angular/common/http";
+import { ElementGetterService } from "./element-getter.service";
+import { AtomicBannerComponent } from './atomic-banner/atomic-banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PeriodicTableComponent,
+    AtomicBannerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ElementGetterService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
