14614bdee77950140c3cb9e645c9d0b4f9c6944e
mart610k <martin-baage@hotmail.com>
2020-08-25 13:20:41 +0200

    process on getting html part of the code.

--- src/app/periodic-table/periodic-table.component.ts
+++ src/app/periodic-table/periodic-table.component.ts
@@ -11,19 +11,57 @@ export class PeriodicTableComponent implements OnInit {
 
   elements$: Element[] = [];
   constructor(private elementService: ElementGetterService) {
+    
   }
 
+  getRowOfElements(row: number): Element[] {
+    let returnTable = [].constructor(18);
+    switch (row) {
+      case 1:
+        returnTable[0] = this.elements$[0]
+        returnTable[17] = this.elements$[1];
+        break;
+      case 2:
+        returnTable[0] = this.elements$[2];
+        returnTable[1] = this.elements$[3];
+        for (let i = 0; i < 6; i++) {
+          returnTable[12 + i] = this.elements$[i + 4];
+        }
+        break;
+      case 3:
+      returnTable[0] = this.elements$[10];
+      returnTable[1] = this.elements$[11];
+      for (let i = 0; i < 6; i++) {
+        returnTable[12 + i] = this.elements$[i + 12];
+      }
+      break;
+    }
+    return returnTable;
+  }
+
+  logout(element: Element) {
+    let classes = []
+
+    if (element !== undefined) {
+      classes.push("col");
+      classes.push(this.getTestClass(element.groupBlock))
+    }
+    else {
+      classes.push("ghost-col")
+    }
+    return classes;
+
+  }
   ngOnInit() {
     return this.elementService.getElements().subscribe(data => this.elements$ = data)
   }
 
-  onClickOnElement(elementAtomicNumber){
+  onClickOnElement(elementAtomicNumber) {
     console.log(elementAtomicNumber);
   }
 
-  getTestClass(groupBlock : string){
-
+  getTestClass(groupBlock: string) {
     let regex = new RegExp(/ /, 'gm');
-    return "style-" + groupBlock.replace(regex,"-");;
+    return "style-" + groupBlock.replace(regex, "-");;
   }
 }
